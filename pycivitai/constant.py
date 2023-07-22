d9540e5ef2d1cc4929d9e772dd31b36854a7b366
narugo1992 <narugo@126.com>
2023-07-22 18:48:09 +0800

    dev(narugo): add utils inside

---
+++ pycivitai/constant.py
@@ -0,0 +1,5 @@
+import os
+
+STORAGE_DIR = os.environ.get('CIVITAI_HOME', os.path.expanduser('~/.cache/civitai'))
+
+ENDPOINT = 'https://civitai.com'
---
+++ pycivitai/utils/__init__.py
@@ -0,0 +1,3 @@
+from .cli import print_version, GLOBAL_CONTEXT_SETTINGS
+from .download import download_file
+from .session import get_requests_session, srequest
