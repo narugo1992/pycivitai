68906e23b548784107248c389b1a25827c557293
narugo1992 <narugo@126.com>
2023-09-11 23:00:50 +0800

    dev(narugo): allow find model by hash

--- pycivitai/client/__init__.py
+++ pycivitai/client/__init__.py
@@ -1,3 +1,4 @@
 from .http import get_session, configure_http_backend, ENDPOINT, OFFLINE_MODE, OfflineModeEnabled
 from .resource import find_version, find_resource, Resource, ResourceNotFound, ModelNotFound, ModelVersionNotFound, \
-    ResourceDuplicated, ModelVersionDuplicated, ModelFoundDuplicated, find_model, find_model_by_name, find_model_by_id
+    ResourceDuplicated, ModelVersionDuplicated, ModelFoundDuplicated, find_model, find_model_by_name, find_model_by_id, \
+    find_version_id_by_hash
--- test/test_dispatch.py
+++ test/test_dispatch.py
@@ -87,3 +87,26 @@ class TestDispatch:
         file = civitai_download('明日方舟-安洁莉娜,Arknights-Angeline')
         assert os.path.getsize(file) == 37863532
         assert calculate_sha256(file) == '89A4D7CF20C76AFEE5AA4C4F28908A05D5E6BB6EB7726E5D319DE74731D09701'
+
+    def test_civitai_find_online_by_hash(self):
+        resource = civitai_find_online('FB64F545')
+        assert resource.model_id == 121986
+        assert resource.model_name == 'mutsuki/浅黄ムツキ/睦月 (Blue Archive)'
+        assert resource.version_id == 155681
+        assert resource.version_name == 'v1.4'
+        assert resource.creator == 'narugo1992'
+        assert resource.filename == 'mutsuki_bluearchive.pt'
+        assert resource.crc32 == 'FB64F545'
+        assert not resource.is_primary
+        assert resource.size == 13278
+
+        resource = civitai_find_online('B42B09FF12')
+        assert resource.model_id == 6755
+        assert resource.model_name == 'Cetus-Mix'
+        assert resource.version_id == 78676
+        assert resource.version_name == 'V4'
+        assert resource.creator == 'Eagelaxis'
+        assert resource.filename == 'cetusMix_v4.safetensors'
+        assert resource.crc32 == '838408E0'
+        assert resource.is_primary
+        assert resource.size == 3894258133
